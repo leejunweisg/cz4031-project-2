3bc7cf04554be20b8e50a91aef4d095c4932334b
dannyyys <yeowys95@gmail.com>
2022-11-07 22:58:51 +0800

    refactor: :recycle: Update explaination

--- app/explain.py
+++ app/explain.py
@@ -6,51 +6,51 @@ def bold_string(string):
 
 
 def aggregate_explain(query_plan):
-    # For plans of the aggregate type: SortAggregate, HashAggregate, PlainAggregate
     strategy = query_plan["Strategy"]
     if strategy == "Sorted":
-        result = "The rows are sorted based on their keys."
+        result = f"{bold_string('Aggregate')} operation is perfomed on the rows based on their keys."
         if "Group Key" in query_plan:
-            result += f" They are {bold_string('aggregated')} by the following keys: "
+            result += f" The {bold_string('aggregated')} key(s) are: "
             for key in query_plan["Group Key"]:
                 result += bold_string(key) + ","
-            result = result[:-1]
-            result += "."
+            result = result[:-1] + "."
         if "Filter" in query_plan:
-            result += " They are filtered by " + bold_string(
+            result += " The rows are also filtered by " + bold_string(
                 query_plan["Filter"].replace("::text", "")
             )
             result += "."
         return result
     elif strategy == "Hashed":
-        result = "It hashes all rows based on the following key(s): "
+        result = f"{bold_string('Aggregate')} operation is performed by hashing on all rows based on the following key(s): "
         for key in query_plan["Group Key"]:
             result += bold_string(key.replace("::text", "")) + ", "
-        result += f"which are then {bold_string('aggregated')} into bucket given by the hashed key."
+        result += f"then the results are {bold_string('aggregated')} into bucket(s) accordung to the hashed key."
         return result
     elif strategy == "Plain":
-        return f"Result is simply {bold_string('aggregated')} as normal."
+        return (
+            f"Normal {bold_string('Aggregate')} operation is performed on the result."
+        )
     else:
-        raise ValueError("Aggregate_explain does not work for strategy: " + strategy)
+        return "Aggregation is performed."
 
 
-def append_explain(query_plan):
-    result = "This plan simply runs multiple sub-operations, and returns all the rows that were returned as one resultset."
+def append_explain():
+    result = f"{bold_string('Append')} operation is performed with multiple sub-operations. All the rows that are returned as one set as the result."
     return result
 
 
 def cte_explain(query_plan):
     result = (
-        f"A {bold_string('CTE scan')} is performed on the table "
+        f"A {bold_string('CTE scan')} operation is performed on the table "
         + bold_string(str(query_plan["CTE Name"]))
         + " which is stored in memory "
     )
     if "Index Cond" in query_plan:
-        result += " with condition(s) " + bold_string(
+        result += " with the condition(s) " + bold_string(
             query_plan["Index Cond"].replace("::text", "")
         )
     if "Filter" in query_plan:
-        result += " and then filtered by " + bold_string(
+        result += " the result is then filtered by " + bold_string(
             query_plan["Filter"].replace("::text", "")
         )
     result += "."
@@ -58,13 +58,13 @@ def cte_explain(query_plan):
 
 
 def function_scan_explain(query_plan):
-    return "The function {} is run and returns the recordset created by it.".format(
+    return "The function {} is run and returns all the recordset(s) that it created.".format(
         bold_string(query_plan["Function Name"])
     )
 
 
 def group_explain(query_plan):
-    result = f"The result from the previous operation is {bold_string('grouped')} together using the following keys: "
+    result = f"The result from the previous operation is {bold_string('grouped')} by the following key(s): "
     for i, key in enumerate(query_plan["Group Key"]):
         result += bold_string(key.replace("::text", ""))
         if i == len(query_plan["Group Key"]) - 1:
@@ -77,14 +77,15 @@ def group_explain(query_plan):
 def index_scan_explain(query_plan):
     result = ""
     result += (
-        f"An {bold_string('index scan')} is done using an index table "
+        f"{bold_string('Index Scan')} operation is performed using "
         + bold_string(query_plan["Index Name"])
+        + " index table "
     )
     if "Index Cond" in query_plan:
-        result += " with the following conditions: " + bold_string(
+        result += " with the following condition(s): " + bold_string(
             query_plan["Index Cond"].replace("::text", "")
         )
-    result += ", and the {} table and fetches rows pointed by indices matched in the scan.".format(
+    result += ", and the {} table and fetches rows that matches the conditions.".format(
         bold_string(query_plan["Relation Name"])
     )
 
@@ -99,17 +100,19 @@ def index_scan_explain(query_plan):
 
 def index_only_scan_explain(query_plan):
     result = ""
-    result += "An index scan is done using an index table " + bold_string(
-        query_plan["Index Name"]
+    result += (
+        f"An {bold_string('Index Scan')} operation is done using "
+        + bold_string(query_plan["Index Name"])
+        + " index table"
     )
     if "Index Cond" in query_plan:
-        result += " with condition(s) " + bold_string(
+        result += " with the condition(s) " + bold_string(
             query_plan["Index Cond"].replace("::text", "")
         )
-    result += ". It then returns the matches found in index table scan as the result."
+    result += ". Matches are then returned as the result."
     if "Filter" in query_plan:
         result += (
-            " The result is then filtered by "
+            " The result is finally filtered by: "
             + bold_string(query_plan["Filter"].replace("::text", ""))
             + "."
         )
@@ -118,42 +121,37 @@ def index_only_scan_explain(query_plan):
 
 
 def limit_explain(query_plan):
-    # https://www.depesz.com/2013/05/09/explaining-the-unexplainable-part-3/
-    result = f"Instead of scanning the entire table, the scan is done with a {bold_string('limit')} of {query_plan['Plan Rows']} entries."
+    result = f"A scan is performed with a {bold_string('limit')} of {query_plan['Plan Rows']} entries."
     return result
 
 
-def materialize_explain(query_plan):
-    # https://www.depesz.com/2013/05/09/explaining-the-unexplainable-part-3/
-    result = "The results of sub-operations are stored in memory for faster access."
+def materialize_explain():
+    result = "The results of previous operation(s) are stored in physical memory/disk for faster access."
     return result
 
 
-def unique_explain(query_plan):
-    result = f"Using the sorted data from the sub-operations, a scan is done on each row and only {bold_string('unique')} values are preserved."
+def unique_explain():
+    result = f"A scan is performed on previous results to remove {bold_string('un-unique')} values."
     return result
 
 
 def merge_join_explain(query_plan):
-    result = (
-        f"The results from sub-operations are joined using {bold_string('Merge Join')}"
-    )
+    result = f"{bold_string('Merge Join')} operation is performed on results from sub-operations"
 
     if "Merge Cond" in query_plan:
-        result += " with condition " + bold_string(
+        result += " on the condition " + bold_string(
             query_plan["Merge Cond"].replace("::text", "")
         )
 
     if "Join Type" == "Semi":
-        result += " but only the row from the left relation is returned"
+        result += " but only the rows from the left relation is returned as the result"
 
     result += "."
     return result
 
 
 def setop_explain(query_plan):
-    # https://www.depesz.com/2013/05/19/explaining-the-unexplainable-part-4/
-    result = "It finds the "
+    result = "Results are returned base on the"
     cmd_name = bold_string(str(query_plan["Command"]))
     if cmd_name == "Except" or cmd_name == "Except All":
         result += "differences "
@@ -168,27 +166,25 @@ def setop_explain(query_plan):
     return result
 
 
-def subquery_scan_explain(query_plan):
-    result = f"{bold_string('Subquery scan')} is done on results from sub-operations but there are no changes."
+def subquery_scan_explain():
+    result = f"{bold_string('Subquery scan')} operation is performed on results from sub-operations without any changes."
     return result
 
 
-def values_scan_explain(query_plan):
-    result = (
-        f"A {bold_string('values scan')} is done using the given values from the query."
-    )
+def values_scan_explain():
+    result = f"A {bold_string('Values Scan')} operation is performed using the values given in query."
     return result
 
 
 def seq_scan_explain(query_plan):
-    sentence = f"It does a {bold_string('sequential scan')} on relation "
+    sentence = f"{bold_string('Sequential Scan')} operation is performed on relation "
     if "Relation Name" in query_plan:
         sentence += bold_string(query_plan["Relation Name"])
     if "Alias" in query_plan:
         if query_plan["Relation Name"] != query_plan["Alias"]:
-            sentence += " with an alias of {}".format(query_plan["Alias"])
+            sentence += " with the alias of {}".format(query_plan["Alias"])
     if "Filter" in query_plan:
-        sentence += " and filtered with the condition {}".format(
+        sentence += " and filtered by {}".format(
             query_plan["Filter"].replace("::text", "")
         )
     sentence += "."
@@ -196,22 +192,22 @@ def seq_scan_explain(query_plan):
     return sentence
 
 
-def nested_loop_explain(query_plan):
-    result = f"The join results between the {bold_string('nested loop')} scans of the suboperations are returned as new rows."
+def nested_loop_explain():
+    result = f"The join results between the {bold_string('Nested Loop')} scans of the suboperations are returned."
     return result
 
 
 def sort_explain(query_plan):
-    result = f"The result is {bold_string('sorted')} using the attribute "
+    result = f"The result is {bold_string('Sorted')} using the attribute "
     if "DESC" in query_plan["Sort Key"]:
         result += (
             bold_string(str(query_plan["Sort Key"].replace("DESC", "")))
-            + " in descending order"
+            + " in descending order of "
         )
     elif "INC" in query_plan["Sort Key"]:
         result += (
             bold_string(str(query_plan["Sort Key"].replace("INC", "")))
-            + " in increasing order"
+            + " in ascending order of "
         )
     else:
         result += bold_string(str(query_plan["Sort Key"]))
@@ -219,14 +215,13 @@ def sort_explain(query_plan):
     return result
 
 
-def hash_explain(query_plan):
-    result = f"The {bold_string('hash')} function makes a memory {bold_string('hash')} with rows from the source."
+def hash_explain():
+    result = f"{bold_string('Hash')} function is used to make a memory {bold_string('hash')} using the table rows."
     return result
 
 
 def hash_join_explain(query_plan):
-    result = ""
-    result += f"The result from previous operation is joined using {bold_string('Hash')} {bold_string(query_plan['Join Type'])} {bold_string('Join')}"
+    result = f"The result from previous operation is joined using {bold_string('Hash')} {bold_string(query_plan['Join Type'])} {bold_string('Join')}"
     if "Hash Cond" in query_plan:
         result += " on the condition: {}".format(
             bold_string(query_plan["Hash Cond"].replace("::text", ""))
@@ -240,7 +235,7 @@ def explain(query_plan):
         case "Aggregate":
             return aggregate_explain(query_plan)
         case "Append":
-            return append_explain(query_plan)
+            return append_explain()
         case "CTE Scan":
             return cte_explain(query_plan)
         case "Function Scan":
@@ -254,25 +249,25 @@ def explain(query_plan):
         case "Limit":
             return limit_explain(query_plan)
         case "Materialize":
-            return materialize_explain(query_plan)
+            return materialize_explain()
         case "Unique":
-            return unique_explain(query_plan)
+            return unique_explain()
         case "Merge Join":
             return merge_join_explain(query_plan)
         case "SetOp":
             return setop_explain(query_plan)
         case "Subquery Scan":
-            return subquery_scan_explain(query_plan)
+            return subquery_scan_explain()
         case "Values Scan":
-            return values_scan_explain(query_plan)
+            return values_scan_explain()
         case "Seq Scan":
             return seq_scan_explain(query_plan)
         case "Nested Loop":
-            return nested_loop_explain(query_plan)
+            return nested_loop_explain()
         case "Sort":
             return sort_explain(query_plan)
         case "Hash":
-            return hash_explain(query_plan)
+            return hash_explain()
         case "Hash Join":
             return hash_join_explain(query_plan)
         case _:

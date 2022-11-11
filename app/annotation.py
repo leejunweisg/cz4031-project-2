5b06c7f666af9d05a20759abc473cc7c1aefd941
dannyyys <yeowys95@gmail.com>
2022-11-11 21:26:57 +0800

    refactor: :recycle: update plan comparison

--- app/annotation.py
+++ app/annotation.py
@@ -1,35 +1,6 @@
 from collections import deque
 
 
-def get_graph_data(plan):
-    inter_result = {"text": {"name": ""}}
-
-    # get all key:value pair with string value
-    for attr, value in plan.items():
-        if isinstance(value, str):
-            if (
-                attr.find("Alias")
-                == -1 & attr.find("Parent")
-                == -1 & attr.find("Direction")
-                == -1
-            ):
-                inter_result["text"]["name"] += value + " "
-
-    # remove last space from string
-    inter_result["text"]["name"] = inter_result["text"]["name"][:-1]
-
-    children_details = []
-
-    if plan.get("Plans") is not None:
-        for plan in plan.get("Plans"):
-            children_details.append(get_graph_data(plan))
-
-    if len(children_details) != 0:
-        inter_result["children"] = children_details
-
-    return inter_result
-
-
 def get_plan_summary(plan):
     """
     Produces a summary given a query plan.
